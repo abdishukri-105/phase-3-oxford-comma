97fda9280407f83a4663ade33733197b78380311
abdishukri <abdishukrimohamed105@gmail.com>
2023-02-08 08:17:33 +0300

    passed all tests

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+    if array.length == 1
+       array[0]
+    elsif array.length == 2
+      array.join(" and ")
+    else
+      last_element = array.pop
+        array.join(", ") + ", and " + last_element
+    end
 end
+  
+
+binding.pry
+0
